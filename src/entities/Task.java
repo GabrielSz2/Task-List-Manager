ec9f5c25103f6c4922945ee0cee96be84dab46c6
GabrielSz2 <gsouzaribeiro21@gmail.com>
2024-06-27 11:44:50 -0400

    projected finish

--- src/entities/Task.java
+++ src/entities/Task.java
@@ -1,31 +1,38 @@
 package entities;
 
-import entities.enums.Stats;
-
 public class Task {
 
-	protected String task;
-	protected Stats stats;
+	private String task;
+	private Boolean stats;
 	
-	public Task(String task, Stats stats) {
+	public Task(String task) {
 		this.task = task;
-		this.stats = stats;
+		this.stats = false;
 	}
 
 	public String getTask() {
 		return task;
 	}
 
-	public void setTask(String task) {
-		this.task = task;
-	}
-
-	public Stats getStats() {
+	public boolean getStats() {
 		return stats;
 	}
 
-	public void setStats(Stats stats) {
+	public void obtainTask(String task, Boolean stats){
+		this.task = task;
 		this.stats = stats;
+	}
+	
+	public void concluded() {
+		stats = true;
+	}
+	
+	@Override
+	public String toString() {
+		if (stats == true) {
+			return task + " [concluido]";
+		}
+		return task + " [pendente]";	
 	}
 	
 }
--- src/taskList/App.java
+++ src/taskList/App.java
@@ -1,20 +1,109 @@
 package taskList;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
-import entities.Options;
+import entities.Task;
 
 public class App {
+	
+	private static List <Task> list = new ArrayList<>();
+	private static Scanner sc = new Scanner(System.in);
+	private static Task ts = new Task(null); 
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		Options op = null;
-		
-		
-		
-		
 
+		menu();
+		
+		int option;
+		
+		do {
+			System.out.print("\n");
+			System.out.print("Escolha uma opção: ");
+			option = sc.nextInt();
+			
+			switch(option) {
+				case 1:
+					addTask();
+					
+					break;
+				case 2:
+					removeTask();
+					
+					break;
+				case 3:
+					markAsCompleted();
+					
+					break;
+				case 4:
+					displayTask();
+					
+					break;
+				case 5:	
+					exit();
+				
+					break;
+				default:
+					System.out.println("Opção invalida!");
+					
+					break;
+			}
+		
+		} while(option != 5);
+		
+		sc.close();
 	}
-
+	
+	private static void menu() {
+		System.out.println("Bem vindo ao gerenciador de tarefas!\n");
+		System.out.println("Menu: ");
+		System.out.println("1. Adicionar tarefa");
+		System.out.println("2. Remover tarefa");
+		System.out.println("3. Marcar tarefa com concluida");
+		System.out.println("4. Exibir tarefas");
+		System.out.println("5. Sair");
+	}
+	
+	private static void addTask() {
+		System.out.print("Digite a descrição da tarefa: ");
+		sc.nextLine();
+		String task = sc.nextLine();
+		list.add(new Task(task));
+		System.out.println("Tarefa adicionado com sucesso!");
+	}
+	
+	private static void removeTask() {
+		System.out.print("qual o indice da tarefa que deseja remover: ");
+		int ind = sc.nextInt();
+		list.remove(ind);
+		System.out.println("Tarefa removida com sucesso!");
+	}
+	
+	private static void markAsCompleted() {
+		System.out.print("Qual o indice da tarefa que deseja marcar como concluida: ");
+		int ind = sc.nextInt();
+		list.get(ind).concluded();
+		System.out.println("A tarefa (" + ind + ") foi marcada como concluida!");
+	}
+	
+	private static void displayTask() {
+		if (list.size() == 0) {
+			System.out.println("Nenhuma tarefa no momento!");
+		}
+		else {
+			System.out.println("Tarefas: ");
+			for (int i=0; i < list.size(); i++) {
+				System.out.println(i + ": " + list.get(i).toString());
+			}
+		}
+	}
+	
+	private static void exit() {
+		System.out.println("Saindo...");
+	}
+	
 }

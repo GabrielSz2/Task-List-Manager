87447bb75b36deb9f506f214135ae370369b1a2b
GabrielSz2 <gsouzaribeiro21@gmail.com>
2024-06-28 09:30:35 -0400

    improvement

--- src/taskList/App.java
+++ src/taskList/App.java
@@ -11,132 +11,125 @@ import java.util.Scanner;
 import entities.Task;
 
 public class App {
-	
-	private static List <Task> list = new ArrayList<>();
-	private static Scanner sc = new Scanner(System.in);
-	private static Task ts = new Task(null); 
 
-	public static void main(String[] args) {
-		
-		Locale.setDefault(Locale.US);
+    private static List<Task> list = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
 
-		menu();
-		
-		int option;
-		
-		do {
-			System.out.print("\n");
-			System.out.print("Escolha uma opção: ");
-			option = sc.nextInt();
-			
-			switch(option) {
-				case 1:
-					addTask();
-					
-					break;
-				case 2:
-					removeTask();
-					
-					break;
-				case 3:
-					markAsCompleted();
-					
-					break;
-				case 4:
-					displayTask();
-					
-					break;
-				case 5:	
-					exit();
-				
-					break;
-				default:
-					System.out.println("Opção invalida!");
-					
-					break;
-			}
-		
-		} while(option != 5);
-		
-		sc.close();
-	}
-	
-	private static void menu() {
-		System.out.println("Bem vindo ao gerenciador de tarefas!\n");
-		System.out.println("Menu: ");
-		System.out.println("1. Adicionar tarefa");
-		System.out.println("2. Remover tarefa");
-		System.out.println("3. Marcar tarefa com concluida");
-		System.out.println("4. Exibir tarefas");
-		System.out.println("5. Sair");
-	}
-	
-	private static void addTask() {
-		System.out.print("Digite a descrição da tarefa: ");
-		sc.nextLine();
-		String task = sc.nextLine();
-		list.add(new Task(task));
-		System.out.println("Tarefa adicionado com sucesso!");
-	}
-	
-	private static void removeTask() {
-		System.out.print("qual o indice da tarefa que deseja remover: ");
-		int ind = sc.nextInt();
-		list.remove(ind);
-		System.out.println("Tarefa removida com sucesso!");
-	}
-	
-	private static void markAsCompleted() {
-		System.out.print("Qual o indice da tarefa que deseja marcar como concluida: ");
-		int ind = sc.nextInt();
-		list.get(ind).concluded();
-		System.out.println("A tarefa (" + ind + ") foi marcada como concluida!");
-	}
-	
-	private static void displayTask() {
-		if (list.size() == 0) {
-			System.out.println("Nenhuma tarefa no momento!");
-		}
-		else {
-			System.out.println("Tarefas: ");
-			for (int i=0; i < list.size(); i++) {
-				System.out.println((i+1) + ": " + list.get(i).toString());
-			}
-		}
-	}
-	
-	private static void exit() {
-		System.out.print("Deseja salvar em um bloco de notas na area de trabalho? (responda com:[y/n]): ");
-		char c = sc.next().charAt(0);
-		
-		if (c == 'y' || c == 'Y') {
-			System.out.println();
-			
-			System.out.print("Digite o nome para o bloco de notas: ");
-			sc.nextLine();
-			String note = sc.nextLine();
-			
-			String path = "C:\\Users\\gabriel.souza\\Desktop\\" + note + ".txt";
-			
-			try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
-				bw.write("Tarefas: \n");
-				int piu=0;
-				for(Task x : list) {
-					bw.write((piu +1) + ": " + x.toString());
-					bw.newLine();
-					piu += 1;
-				}
-			}
-			catch(IOException k) {
-				k.printStackTrace();
-			}	
-			System.out.println("Salvando e saindo...");
-			
-		}else {
-			System.out.println("Saindo...");
-		}
-		
-		
-	}
-	
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        menu();
+
+        int option;
+
+        do {
+            System.out.print("\n");
+            System.out.print("Escolha uma opção: ");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    addTask();
+                    break;
+                case 2:
+                    removeTask();
+                    break;
+                case 3:
+                    markAsCompleted();
+                    break;
+                case 4:
+                    displayTask();
+                    break;
+                case 5:
+                    exit();
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+
+        } while (option != 5);
+
+        sc.close();
+    }
+
+    private static void menu() {
+        System.out.println("Bem-vindo ao gerenciador de tarefas!\n");
+        System.out.println("Menu: ");
+        System.out.println("1. Adicionar tarefa");
+        System.out.println("2. Remover tarefa");
+        System.out.println("3. Marcar tarefa como concluída");
+        System.out.println("4. Exibir tarefas");
+        System.out.println("5. Sair");
+    }
+
+    private static void addTask() {
+        System.out.print("Digite a descrição da tarefa: ");
+        sc.nextLine();
+        String task = sc.nextLine();
+        list.add(new Task(task));
+        System.out.println("Tarefa adicionada com sucesso!");
+    }
+
+    private static void removeTask() {
+        System.out.print("Qual o índice da tarefa que deseja remover: ");
+        int ind = sc.nextInt() - 1;  // Ajustar para índice base 0
+        if (ind >= 0 && ind < list.size()) {
+            list.remove(ind);
+            System.out.println("Tarefa removida com sucesso!");
+        } else {
+            System.out.println("Índice inválido!");
+        }
+    }
+
+    private static void markAsCompleted() {
+        System.out.print("Qual o índice da tarefa que deseja marcar como concluída: ");
+        int ind = sc.nextInt() - 1;  // Ajustar para índice base 0
+        if (ind >= 0 && ind < list.size()) {
+            list.get(ind).concluded();
+            System.out.println("A tarefa (" + (ind + 1) + ") foi marcada como concluída!");
+        } else {
+            System.out.println("Índice inválido!");
+        }
+    }
+
+    private static void displayTask() {
+        if (list.size() == 0) {
+            System.out.println("Nenhuma tarefa no momento!");
+        } else {
+            System.out.println("Tarefas: ");
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i + 1) + ": " + list.get(i).toString());
+            }
+        }
+    }
+
+    private static void exit() {
+        System.out.print("Deseja salvar em um bloco de notas na área de trabalho? (responda com:[y/n]): ");
+        char c = sc.next().charAt(0);
+
+        if (c == 'y' || c == 'Y') {
+            System.out.println();
+
+            System.out.print("Digite o nome para o bloco de notas: ");
+            sc.nextLine();  // Consumir a quebra de linha
+            String note = sc.nextLine();
+
+            String path = "C:\\Users\\gabriel.souza\\Desktop\\" + note + ".txt";
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+                bw.write("Tarefas: \n");
+                for (int i = 0; i < list.size(); i++) {
+                    bw.write((i + 1) + ": " + list.get(i).toString());
+                    bw.newLine();
+                }
+            } catch (IOException k) {
+                k.printStackTrace();
+            }
+            System.out.println("Salvando e saindo...");
+        } else {
+            System.out.println("Saindo...");
+        }
+    }
 }
